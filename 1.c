5f4c80098c48985cc487d20cd9f8eb776fb8fba2
laiguisheng <3044170061@qq.com>
2025-10-23 16:48:16 +0800

    1

--- 1.c
+++ 1.c
@@ -1,8 +1,9 @@
-// 1.c
-#include <stdio.h>
-
-// 标准的main函数声明（必须返回int，参数列表明确为void）
-int main(void) {  
-    printf("你好世界chcp\n");
-    return 0;  // 明确返回0（符合int类型要求）
+#include<stdio.h>
+int main(void)
+{
+    int a,b;
+    printf("请输入两个整数，两个整数中间用空格分隔：");
+    scanf("%d%d",&a,&b);
+    printf("%d+%d=%d",a,b,a+b);
+    return 0;
 }
